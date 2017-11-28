0df8bedc2160ff88d9718efdd977dd94601a2e37
Obtuse <theobtuseautodidact@gmail.com>
2017-11-28 11:32:52 -0700

    Finish section 8: Intro to JS

---
+++ js_lesson/js_stalker_script.js
@@ -0,0 +1,7 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("What is your age?");
+var fullName = firstName + " " + lastName;
+
+alert("User's name is " + fullName + ".");
+console.log(fullName + " is " + age + " years old.");
