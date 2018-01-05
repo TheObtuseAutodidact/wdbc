3d102d94a01699afe893ecf90a75c4a91ecd7313
Obtuse <theobtuseautodidact@gmail.com>
2018-01-04 21:49:07 -0700

    Second pass on js console todo list

--- js_lesson/todo_list/todo_list.js
+++ js_lesson/todo_list/todo_list.js
@@ -2,15 +2,35 @@ var todos = [];
 
 var input = prompt("What you you like to do?");
 
-
 while (input !== "quit") {
 
     if (input === "list") {
-        console.log(todos);
+        listTodos();
     } else if (input === "new") {
-        var newTodo = prompt("Enter new Todo");
-        todos.push(newTodo);
+        addTodo();
+    } else if (input === "delete") {
+        deleteTodo();
     }
     input = prompt("What you you like to do?");
 }
 console.log("OK, You Quit The App");
+
+function listTodos() {
+    console.log("******")
+    todos.forEach(function(todo, index){
+    console.log(index + ": " + todo);
+    });
+    console.log("******");
+}
+
+function addTodo() {
+    var newTodo = prompt("Enter new Todo");    
+    todos.push(newTodo);
+    console.log("Added todo");
+}
+
+function deleteTodo() {
+    idx = prompt("Enter the index of todo to delete");
+    todos.splice(idx, 1);
+    console.log("Deleted todo");
+}
