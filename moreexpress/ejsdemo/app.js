fe28afddc97fe2b3891aeb1cc77221c8186d9b44
Obtuse <theobtuseautodidact@gmail.com>
2019-02-28 11:10:33 -0700

    Express Demo App - GET requests

--- moreexpress/ejsdemo/app.js
+++ moreexpress/ejsdemo/app.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"));
+// app.set("view engine", "ejs"); // this allows rendered file to be specified with out ".ejs" suffix
+
 app.get("/", function(req, res) {
   res.render("home.ejs");
 })
@@ -8,6 +11,16 @@ app.get("/", function(req, res) {
 app.get("/fallinlovewith/:thing", function(req, res) {
   var thing = req.params.thing;
   res.render("love.ejs", {thingVar: thing});
+})
+
+app.get("/posts", function(req, res) {
+  var posts = [
+    {title: "Post 1", author: "Susy"},
+    {title: "My adorable pet bunny", author: "Charlie"},
+    {title: "Can you believe this pomsky?", author: "Colt"},
+  ];
+
+  res.render("posts.ejs", {posts: posts});
 })
 
 
